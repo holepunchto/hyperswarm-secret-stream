3233a0de06831c29c9b61d8bb553d170112062a0
Mathias Buus <mathiasbuus@gmail.com>
2021-04-14 15:16:32 +0200

    add tcp test

--- test.js
+++ test.js
@@ -1,4 +1,5 @@
 const tape = require('tape')
+const net = require('net')
 const NoiseStream = require('./')
 
 tape('basic', function (t) {
@@ -17,3 +18,29 @@ tape('basic', function (t) {
     t.same(a.publicKey, b.remotePublicKey)
   })
 })
+
+tape('works with external streams', function (t) {
+  const server = net.createServer(function (socket) {
+    const s = new NoiseStream(false, socket)
+
+    s.on('data', function (data) {
+      s.destroy()
+      t.same(data, Buffer.from('encrypted!'))
+    })
+  })
+
+  server.listen(0, function () {
+    const socket = net.connect(server.address().port)
+    const s = new NoiseStream(true, socket)
+
+    s.write(Buffer.from('encrypted!'))
+    s.on('close', function () {
+      server.close()
+    })
+  })
+
+  server.on('close', function () {
+    t.end()
+  })
+})
+
